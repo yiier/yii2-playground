0e1e825c11f7d8939cab212e338b0d65fe4db74c
forecho <caizhenghai@gmail.com>
2015-05-11 15:35:28 +0800

    添加测试代码

--- common/config/main.php
+++ common/config/main.php
@@ -5,13 +5,6 @@ return [
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
-        'beanstalk'    => [
-            'class'          => 'udokmeci\yii2beanstalk\Beanstalk',
-            'host'           => "127.0.0.1", // default host
-            'port'           => 11300, //default port
-            'connectTimeout' => 1,
-            'sleep'          => false, // or int for usleep after every job
-        ],
     ],
     'controllerMap' => [
         'worker'=>[
--- frontend/modules/queue/controllers/DefaultController.php
+++ frontend/modules/queue/controllers/DefaultController.php
@@ -8,6 +8,11 @@ class DefaultController extends Controller
 {
     public function actionIndex()
     {
+        $r1 = rand(1000, 9999);
+        $r2 = rand(1000, 9999);
+        $mixedData = "{{$r1}:{$r2}}";
+        $tube = \Yii::$app->beanstalk->putInTube('tube', $mixedData);
+        var_dump($tube);
         return $this->render('index');
     }
 }

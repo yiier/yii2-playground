82a3919e7a0e869dc5ca446e5c77773d6e2fa14d
yiqing <yiqing_95@qq.com>
2015-05-24 23:59:55 +0800

    准备添加yii2-form-builder 扩展用例

---
+++ frontend/modules/ui/controllers/Yii2FormBuilderController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace frontend\modules\ui\controllers;
+
+class Yii2FormBuilderController extends \yii\web\Controller
+{
+    public function actionIndex()
+    {
+        return $this->render('index');
+    }
+
+}
